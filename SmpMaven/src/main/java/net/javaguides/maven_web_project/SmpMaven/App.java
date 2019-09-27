e5c2d887ede5db883892d00cd4366e27c34364d2
saviosabino <saviopp@gmail.com>
2019-09-27 12:34:28 -0300

    desks integration

---
+++ SmpMaven/src/main/java/net/javaguides/maven_web_project/SmpMaven/App.java
@@ -0,0 +1,14 @@
+package net.javaguides.maven_web_project.SmpMaven;
+import com.google.gson.Gson;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+ 
