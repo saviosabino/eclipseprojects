3cf2de90e4b6e017481101abad215b1ee744550d
saviosabino <saviopp@gmail.com>
2019-11-25 17:20:57 -0300

    commit

---
+++ estruturadorTXT/src/estruturadorTXT/PMGinss.java
@@ -0,0 +1,61 @@
+package estruturadorTXT;
+
+public class PMGinss {
+	String matric;
+	String nome;
+	String orgao;
+	String arquivo;
+	String data;
+	String horario;
+	String usuario;
+	String assunto;
+	public String getMatric() {
+		return matric;
+	}
+	public void setMatric(String matric) {
+		this.matric = matric;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getOrgao() {
+		return orgao;
+	}
+	public void setOrgao(String orgao) {
+		this.orgao = orgao;
+	}
+	public String getArquivo() {
+		return arquivo;
+	}
+	public void setArquivo(String arquivo) {
+		this.arquivo = arquivo;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public String getHorario() {
+		return horario;
+	}
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getAssunto() {
+		return assunto;
+	}
+	public void setAssunto(String assunto) {
+		this.assunto = assunto;
+	}
+	
+}
---
+++ estruturadorTXT/src/estruturadorTXT/estruturadorDados.java
@@ -0,0 +1,73 @@
+package estruturadorTXT;
+
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class estruturadorDados {
+	
+	public static List<PMGinss> carregaPMGinss() {
+		List<PMGinss> aposentados = new ArrayList<PMGinss>();
+		
+		try {
+			List<String> lines = Files.readAllLines(Paths.get("apos_ativos_INSS_PMG.txt"));
+			
+			PMGinss aposentado = null;
+	        for (String line : lines) {
+	        	
+	            if(line.startsWith("| Servidor :"))
+	            {
+	            	aposentado = new PMGinss();
+	            	aposentados.add(aposentado);
+	            	aposentado.matric = line.substring(13, 19);
+	            	aposentado.nome = line.split(" Orgao")[0].split("-")[1];
+	            	aposentado.orgao = line.split(" Orgao : ")[1].replace("|", "").trim();
+	                //...add data (name)
+	            }else if(line.startsWith("| Arquivo :")) {
+	            	aposentado.arquivo = line.substring(12,24);
+	            	aposentado.data = line.substring(32,42);
+	            	aposentado.horario = line.substring(53,61);
+	            	aposentado.usuario = line.substring(72).replace("|", "").trim();//.split("|")[0];
+	            	
+	            }else if(line.startsWith("| Assunto :")) {
+	            	aposentado.assunto = line.substring(12).replace("|", "").trim();//.split("|")[0];
+	            }
+	            //...add more data (description and dates)
+	        }
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return aposentados;
+	}
+
+	public static void gravaCSV(List<PMGinss> aposentados) {
+		try {
+			PrintStream out = new PrintStream(new FileOutputStream("apoINSSPMG.csv"));
+			
+			out.println("matric; nome; orgao; arquivo; data; horario; usuario; assunto");
+			for (PMGinss apo : aposentados) {
+				out.println(apo.matric + "; " + apo.nome + "; " + apo.orgao +"; " + apo.arquivo + "; " +
+					apo.data + "; " + apo.horario + "; " + apo.usuario + "; " + apo.assunto);
+			}		    
+			out.close();
+			System.out.println("Arquivo escrito com sucesso!");
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("saida");
+		gravaCSV(carregaPMGinss());
+	}
+}
+
+
